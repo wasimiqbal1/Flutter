ae486de1470e00af88ccfd3b72822ac29dac09a9
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-06 12:15:15 +0500

    flutter MediaQuery

--- firstapp_wasim/lib/home.dart
+++ firstapp_wasim/lib/home.dart
@@ -10,35 +10,11 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Align(
-          alignment: Alignment.bottomLeft,
-                  child: Container(
-              color: Colors.black,height:100, width:100 ),),
-       
-        Positioned(
-          left: 25,
-          top: 25,
-              child: Container(
-                   color: Colors.purple,height:100, width:100,)),
-        
-        
-        Align(
-          alignment: Alignment.bottomRight,
-                 child: Container(
-                   color: Colors.green,height:100, width:100,)),
-        
-        Align(
-          alignment: Alignment.topLeft,
-              child: Container(
-                  color: Colors.yellow,height:100, width:100,)),
-        
-        Align(
-          alignment: Alignment.topRight,
-              child: Container(
-                color: Colors.red,height:100, width:100,)),
-
-      ],)
+      body: Container(
+        color: Colors.purple,
+        height: MediaQuery.of(context).size.height*0.3,
+        width: MediaQuery.of(context).size.width*0.3,
+      )      
     );
   }
 }
