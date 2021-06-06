05aac953bfd1716d361fda7c66fdc27a21b03da4
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-06 11:45:17 +0500

    flutter align

--- firstapp_wasim/lib/home.dart
+++ firstapp_wasim/lib/home.dart
@@ -10,22 +10,28 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-          Align(
-            alignment: Alignment.topCenter,
-                      child: Container(
-              padding: EdgeInsets.only(left: 20, top: 20),
-              child: Text("Box Decoration"),
-              width: 200,
-              height: 200, 
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(20)
+      body: Stack(children: [
+        Align(
+          alignment: Alignment.bottomLeft,
+                  child: Container(
+              color: Colors.black,height:100, width:100 ),),
+       
+        Align(
+          alignment: Alignment.bottomRight,
+                 child: Container(
+                   color: Colors.green,height:100, width:100,)),
+        
+        Align(
+          alignment: Alignment.topLeft,
+              child: Container(
+                  color: Colors.yellow,height:100, width:100,)),
+        
+        Align(
+          alignment: Alignment.topRight,
+              child: Container(
+                color: Colors.red,height:100, width:100,)),
 
-              ),
-              ),
-          )
-        ,
+      ],)
     );
   }
 }
