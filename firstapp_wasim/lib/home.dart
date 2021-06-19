7aa75e0afb164d565a60008e404f18a18587833b
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-19 23:58:08 +0500

    container

--- firstapp_wasim/lib/home.dart
+++ firstapp_wasim/lib/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
-
 
 class Home extends StatefulWidget {
   @override
@@ -13,6 +11,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: ListView(
         children: [
+          Container(color: Colors.yellow, height: 100,)
         ]    
       )      
     );
