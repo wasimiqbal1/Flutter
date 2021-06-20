888bd2921770fa71a3b94ded97d4b2e4aec75f85
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-20 12:40:29 +0500

    Drawer

--- firstapp_wasim/lib/home.dart
+++ firstapp_wasim/lib/home.dart
@@ -10,19 +10,18 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 50,
-        mainAxisSpacing: 50,
-        children: List.generate(list.length, (index) {
-            return Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                color: Colors.cyan,
-                child: Center(child: Text(list[index])),
-              ),
-            );
-        })
-    ));
+      appBar: AppBar(title: Text("Drawer Example"),),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(child: Container(color: Colors.cyan,child: Text("ABC"))),
+            ListTile(title: Text("Hello"),),
+            ListTile(title: Text("Hello"),)
+          ],
+        ),
+  
+  
+  ),
+);
 }
 }
