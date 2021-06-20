6a06e8e8e8fc5460506ac6ce2b94d784ec2a6995
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-20 11:38:25 +0500

    listview builder

--- firstapp_wasim/lib/home.dart
+++ firstapp_wasim/lib/home.dart
@@ -4,7 +4,7 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
-var list = ["https://image.cnbcfm.com/api/v1/image/104836112-p100d-review-8.JPG?v=1510605806","https://i.insider.com/5d308dc19e075556e379ced6?width=600&format=jpeg&auto=webp","https://images.unsplash.com/photo-1517524008697-84bbe3c3fd98?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"];
+var list = ["https://i.insider.com/5d308dc19e075556e379ced6?width=600&format=jpeg&auto=webp","https://image.cnbcfm.com/api/v1/image/104836112-p100d-review-8.JPG?v=1510605806","https://images.unsplash.com/photo-1517524008697-84bbe3c3fd98?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"];
 
 class _HomeState extends State<Home> {
   @override
