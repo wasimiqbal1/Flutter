08731daf1846b375aa8162d30ec6d894e4ff7c2e
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-20 11:22:16 +0500

    subtitle

--- firstapp_wasim/lib/home.dart
+++ firstapp_wasim/lib/home.dart
@@ -5,6 +5,7 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 var list = ["Ahmed","Hashir","Abdul Kabir"];
+var age = ["16","14","7"];
 
 class _HomeState extends State<Home> {
   @override
@@ -15,6 +16,7 @@ class _HomeState extends State<Home> {
         itemBuilder: (context,index){
           return ListTile(
             title: Text(list[index]),
+            subtitle: Text(age[index]),
             );
           })
     );
