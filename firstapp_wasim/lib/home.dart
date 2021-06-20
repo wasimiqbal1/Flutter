e077cb493360d846391c0af22e2c6613e9fa48c7
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-20 12:06:16 +0500

    Grid

--- firstapp_wasim/lib/home.dart
+++ firstapp_wasim/lib/home.dart
@@ -4,21 +4,23 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
-var list = ["https://i.insider.com/5d308dc19e075556e379ced6?width=600&format=jpeg&auto=webp","https://image.cnbcfm.com/api/v1/image/104836112-p100d-review-8.JPG?v=1510605806","https://images.unsplash.com/photo-1517524008697-84bbe3c3fd98?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"];
+var list = ["Wasim","Abdul Kabir","Ahmed","Hashir"];
 
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (context,index){
-          return Container(
-            height: 200,
-            width: 200,
-            child: Image.network(list[index]),
+      body: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 50,
+        mainAxisSpacing: 50,
+        children: List.generate(list.length, (index) {
+            return Container(
+              color: Colors.cyan,
+              child: Center(child: Text(list[index])),
             );
-          })
-    );
+        })
+       
+    ));
 }
 }
