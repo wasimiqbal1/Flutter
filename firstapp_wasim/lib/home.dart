c2b82efe200d3346ca8d3fae2615d96a0e1ba185
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-20 11:35:25 +0500

    Listview builder

--- firstapp_wasim/lib/home.dart
+++ firstapp_wasim/lib/home.dart
@@ -4,8 +4,7 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
-var list = ["Ahmed","Hashir","Abdul Kabir"];
-var age = ["16","14","7"];
+var list = ["https://image.cnbcfm.com/api/v1/image/104836112-p100d-review-8.JPG?v=1510605806","https://i.insider.com/5d308dc19e075556e379ced6?width=600&format=jpeg&auto=webp","https://images.unsplash.com/photo-1517524008697-84bbe3c3fd98?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"];
 
 class _HomeState extends State<Home> {
   @override
@@ -14,9 +13,10 @@ class _HomeState extends State<Home> {
       body: ListView.builder(
         itemCount: list.length,
         itemBuilder: (context,index){
-          return ListTile(
-            title: Text(list[index]),
-            subtitle: Text(age[index]),
+          return Container(
+            height: 200,
+            width: 200,
+            child: Image.network(list[index]),
             );
           })
     );
