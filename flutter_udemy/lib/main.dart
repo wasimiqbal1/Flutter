19c2c659b2c429550c429445ab9c0f7621bbf620
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-27 21:03:15 +0500

    flutter appbar counter

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 void main() {
   runApp(new MaterialApp(
@@ -14,35 +13,32 @@ class MyApp extends StatefulWidget {
 
 class _State extends State<MyApp> {
 
-  String _value = '';
-  
-  Future _selectDate() async {
-    DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: new DateTime.now(),
-        firstDate: new DateTime(2016),
-        lastDate: new DateTime(2099)
-    );
-   if(picked != null) setState(() => _value = picked.toString());     
-  }
+  int _value = 0;
+
+  void _add() => setState(() => _value++);
+  void _remove() => setState(() => _value--);
+
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Flutter App'),
+        title: new Text('Flutter Counter App'),
+        backgroundColor: Colors.lightGreen,
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.add), onPressed: _add),
+          new IconButton(icon: new Icon(Icons.remove), onPressed: _remove)
+        ],
       ),
       body: new Container(
-          padding: new EdgeInsets.all(32.0),
-          child: new Center(
-            child: new Column(
-              children: <Widget>[
-               new Text(_value),
-              new ElevatedButton(onPressed: _selectDate, child: new Text('Click me'),)
-
-              ],
-            ),
-          )
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(_value.toString(), style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 37.0),)
+          ],
+          ),
+        )
       ),
     );
   }
