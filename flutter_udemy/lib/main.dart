2c4e6125820f35be79564c8599b668ebf731cdef
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-27 22:32:17 +0500

    flutter drawer

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -15,21 +15,32 @@ class _State extends State<MyApp> {
 
   String _value = '';
 
-  void _onClick(String value) => setState(() => _value = value);
+  void _onClick() {
+    setState((){
+      _value = new DateTime.now().toString();
+    });
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Flutter Footer Button'),
+        title: new Text('Flutter Drawer App'),
       ),
-
-      persistentFooterButtons: <Widget>[
-        new IconButton(icon: new Icon(Icons.timer), onPressed: () => _onClick('Button 1')),
-        new IconButton(icon: new Icon(Icons.people), onPressed: () => _onClick('Button 2')),
-        new IconButton(icon: new Icon(Icons.map), onPressed: () => _onClick('Button 3')),
-      ],
-
+      
+      drawer: new Drawer(
+        child: new Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Column(
+            children: <Widget>[
+              new Text('Hello Drawer'),
+              new ElevatedButton(onPressed: _onClick, child: new Text('Close Drawer'),)
+            ],
+          ),
+        ),
+      ),
+      
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
