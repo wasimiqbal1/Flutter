cd75b17dbf18ce3a2d306de11b897812d3d45610
wasimiqbal1 <wasim_iqbal@msn.com>
2021-06-27 21:55:53 +0500

    Flutter Footer Button

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -12,30 +12,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  String _value = '';
+
+  void _onClick(String value) => setState(() => _value = value);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Name here'),
+        title: new Text('Flutter Footer Button'),
       ),
-      drawer: new Drawer(
-        child: new Container(
-          padding: new EdgeInsets.all(32.0),
-          child: new Column(
-            children: <Widget>[
-              new Text('Hello Drawer'),
-              new ElevatedButton(onPressed: () => Navigator.pop(context), child: new Text('Close'),)
-            ],
-          ),
-        ),
-      ),
-      
+
+      persistentFooterButtons: <Widget>[
+        new IconButton(icon: new Icon(Icons.timer), onPressed: () => _onClick('Button 1')),
+        new IconButton(icon: new Icon(Icons.people), onPressed: () => _onClick('Button 2')),
+        new IconButton(icon: new Icon(Icons.map), onPressed: () => _onClick('Button 3')),
+      ],
+
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text('Add Widgets Here')
+              new Text(_value)
             ],
           ),
         )
