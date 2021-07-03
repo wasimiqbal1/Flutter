c586b559bdf1ff4ff316cb7d87796e5c3df70361
wasimiqbal1 <wasim_iqbal@msn.com>
2021-07-04 00:07:01 +0500

    flutter listview

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 void main() {
   runApp(new MaterialApp(
     home: new MyApp(),
@@ -12,8 +17,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  Map _countries = new Map();
+
+  void _getData() async {
+    var url = 'http://country.io/names.json';
+    var response = await http.get(url);
+
+    if(response.statusCode == 200) {
+      setState(() => _countries = json.decode(response.body));
+      print('Loaded ${_countries.length} countries');
+    } else {
+      print("Status code: ${response.statusCode}");
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
+    //_getData();
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Name here'),
@@ -23,13 +47,30 @@ class _State extends State<MyApp> {
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text('Image Demo'),
-              new Expanded(child: new Image.network('http://voidrealms.com/images/smile.jpg')),
+              new Text('Countries', style: new TextStyle(fontWeight: FontWeight.bold),),
+              new Expanded(child: new ListView.builder(
+                itemCount: _countries.length,
+                itemBuilder: (BuildContext context, int index){
+                  String key = _countries.keys.elementAt(index);
+                  return new Row(
+                    children: <Widget>[
+                      new Text('${key} : '),
+                      new Text(_countries[key])
+                    ],
+                  );
+                },
+                  
+              ))
 
             ],
           ),
         )
       ),
     );
+  }
+
+  @override
+  void initState() {
+    _getData();
   }
 }
