0aed8dd70e82b0e3be01fb3f79b79f82bb0d11ed
wasimiqbal1 <wasim_iqbal@msn.com>
2021-07-11 00:57:49 +0500

    gridview

--- flutter_calculator_app/lib/home.dart
+++ flutter_calculator_app/lib/home.dart
@@ -7,34 +7,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<dynamic> lst = [1,2,3,4,5,6,7,8];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
               child: Column(
           children: [
-            ListView(
-              physics: NeverScrollableScrollPhysics(),    
+            Container( 
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+              margin: EdgeInsets.only(top: 10),
+            ),
+            Container( 
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+              margin: EdgeInsets.only(top: 10),
+            ),
+            Container( 
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+              margin: EdgeInsets.only(top: 10),
+            ),
+            Container( 
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+              margin: EdgeInsets.only(top: 10),
+            ),
+            Container( 
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+              margin: EdgeInsets.only(top: 10),
+            ),
+ 
+            GridView.count(
               shrinkWrap: true,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top:10,),
-                  color: Colors.amber,
-                  height: 200, 
-                  ),
-                Container(
-                  margin: EdgeInsets.only(top:10,),
-                  color: Colors.blue,
-                  height: 200, 
-                  ),
-                Container(
-                  margin: EdgeInsets.only(top:10,),
-                  color: Colors.pink,
-                  height: 200, 
-                  ),
-              ]
+              crossAxisCount: 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+              children: List.generate(lst.length, (index)  {
+                return Container(
+                  color: Colors.red,
+                  child: Center(child: Text("${lst[index]}"),
+                ));
+            }),
             )
-          ]
+
+         ]
         ),
       ),
     );
