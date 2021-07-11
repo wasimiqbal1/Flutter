d03137e05184abe69fc7043403374eee56e08ac3
wasimiqbal1 <wasim_iqbal@msn.com>
2021-07-11 16:00:41 +0500

    simplecalc

--- flutter_calculator_app/lib/home.dart
+++ flutter_calculator_app/lib/home.dart
@@ -1,4 +1,6 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class Calculator extends StatefulWidget {
  
@@ -8,50 +10,80 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+
+  var result = "";
+
+Widget btn (var textt){
+  return ElevatedButton(onPressed: (){
+    setState(() {
+      result = result + textt;
+    });
+  }, child: Text(textt));
+}
+
+clearr(){
+  setState(() {
+    result = "";
+  });
+}
+
+output(){
+  Parser p = Parser();
+  Expression exp = p.parse(result);
+  ContextModel cm = ContextModel();
+  double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+  setState(() {
+    result = eval.toString();
+  });
+
+}
+
   @override
     Widget build(BuildContext context) {
       return Scaffold(
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-              btn("1"),
-              btn("2"),
-              btn("3"),
-              btn("4")
-          ],),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-              btn("5"),
-              btn("6"),
-              btn("7"),
-              btn("8")
-          ],),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-              btn("9"),
-              btn("0"),
-              btn("+"),
-              btn("-")
-          ],),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-              btn("*"),
-              btn("/"),
-              btn("clear"),
-              btn("=")
-          ],),
-          
-        ]
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(result,style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                btn("1"),
+                btn("2"),
+                btn("3"),
+                btn("4")
+            ],),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                btn("5"),
+                btn("6"),
+                btn("7"),
+                btn("8")
+            ],),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                btn("9"),
+                btn("0"),
+                btn("+"),
+                btn("-")
+            ],),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                btn("*"),
+                btn("/"),
+                ElevatedButton(onPressed: clearr, child: Text("clear")),
+                ElevatedButton(onPressed: output, child: Text("=")),
+            ],),
+            
+          ]
       ),
+     ),
     );
   }
 }
 
-Widget btn (var textt){
-  return ElevatedButton(onPressed: (){}, child: Text(textt));
-}
