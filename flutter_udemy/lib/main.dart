95339085728cbc8a45b3143f971635ed9e30abd0
wasimiqbal1 <wasim_iqbal@msn.com>
2021-07-28 00:00:53 +0500

    Flutter tooltips

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -13,6 +13,10 @@
 
   class _State extends State<MyApp> {
 
+    String _value = 'Nothing Yet';
+    
+    void _onPressed() => setState(() => _value = new DateTime.now().toString());
+
     @override
     Widget build(BuildContext context) {
       return new Scaffold(
@@ -24,8 +28,13 @@
           child: new Center(
             child: new Column(
               children: <Widget>[
-                new Text('Text here'),
-              ],
+              new Text(_value),
+              new IconButton(
+                  icon: new Icon(Icons.timer),
+                  onPressed: _onPressed,
+                tooltip: 'Click me',
+              )
+             ],
             ),
           )
         ),
