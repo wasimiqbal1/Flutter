4d31c3a334454b2ec2f3bc070fa4aef244e290ca
wasimiqbal1 <wasim_iqbal@msn.com>
2021-07-28 00:22:09 +0500

    Slider & Indicator

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -1,43 +1,51 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-  void main() {
-    runApp(new MaterialApp(
-      home: new MyApp(),
-    ));
-  }
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
 
-  class MyApp extends StatefulWidget {
-    @override
-    _State createState() => new _State();
-  }
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
 
-  class _State extends State<MyApp> {
+class _State extends State<MyApp> {
 
-    String _value = 'Nothing Yet';
-    
-    void _onPressed() => setState(() => _value = new DateTime.now().toString());
+  double _value = 0.0;
 
-    @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Flutter App'),
-        ),
-        body: new Container(
-          padding: new EdgeInsets.all(32.0),
-          child: new Center(
-            child: new Column(
-              children: <Widget>[
-              new Text(_value),
-              new IconButton(
-                  icon: new Icon(Icons.timer),
-                  onPressed: _onPressed,
-                tooltip: 'Click me',
+  void _onChanged(double value) => setState(() => _value = value);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Flutter Slider & Indicator'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Slider(value: _value, onChanged: _onChanged),
+              new Container(
+                padding: new EdgeInsets.all(32.0),
+                child:  new LinearProgressIndicator(
+                  value: _value,
+                  valueColor: new AlwaysStoppedAnimation<Color>(Colors.green),
+                ),
+              ),
+              new Container(
+                padding: new EdgeInsets.all(32.0),
+                child:  new CircularProgressIndicator(
+                  value: _value,
+                ),
               )
-             ],
-            ),
-          )
-        ),
-      );
-    }
+            ],
+          ),
+        )
+      ),
+    );
   }
+}
