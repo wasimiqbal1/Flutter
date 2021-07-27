c87dfbf5572d715c91b8fd1a70e0ef78484a5c95
wasimiqbal1 <wasim_iqbal@msn.com>
2021-07-27 23:52:14 +0500

    Update

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -1,43 +1,34 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _State createState() => new _State();
-}
-
-class _State extends State<MyApp> {
-
-  List _pets = ['Cats', 'Cow', 'Goats', 'Birds', 'Dogs'];
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Flutter listView'),
-      ),
-      body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              new Text('Available Pets'),
-              new Expanded(child: new ListView.builder(
-                  itemCount: _pets.length,
-                  itemBuilder: (BuildContext context, int index) {
-                       return new Text(_pets.elementAt(index));
-                  }
-              )),
-
-            ],
-          ),
-        )
-      ),
-    );
+  void main() {
+    runApp(new MaterialApp(
+      home: new MyApp(),
+    ));
   }
-}
+
+  class MyApp extends StatefulWidget {
+    @override
+    _State createState() => new _State();
+  }
+
+  class _State extends State<MyApp> {
+
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter App'),
+        ),
+        body: new Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text('Text here'),
+              ],
+            ),
+          )
+        ),
+      );
+    }
+  }
