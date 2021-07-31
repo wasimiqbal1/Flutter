932ddab622d9a7369046b707c5f16e399809b3ab
wasimiqbal1 <wasim_iqbal@msn.com>
2021-07-31 22:31:50 +0500

    Navigation

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -1,35 +1,23 @@
 import 'package:flutter/material.dart';
-import 'mywidget.dart';
+import './screens/home.dart';
+import './screens/second.dart';
+import './screens/third.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
-}
+void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _State createState() => new _State();
-}
+class MyApp extends StatelessWidget {
 
-class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Name here'),
-      ),
-      body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              new Text('Add Widgets Here'),
-              new MyWidget()
-            ],
-          ),
-        )
-      ),
+    return new MaterialApp(
+      title: 'Navigation',
+      routes: <String, WidgetBuilder>{
+        //All available pages
+        '/Home': (BuildContext contrext) => new Home(),
+        '/Second': (BuildContext contrext) => new Second(),
+        '/Third': (BuildContext contrext) => new Third(),
+      },
+      home: new Home(), //first page displayed
     );
   }
 }
