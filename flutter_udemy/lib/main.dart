43e55e058bb79cb8ef69640b8f6f0c2cef079dba
wasimiqbal1 <wasim_iqbal@msn.com>
2021-08-01 17:29:44 +0500

    Custom Painter

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -11,51 +11,63 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
-class _State extends State<MyApp> {
-
-  late double _x;
-  late double _y;
-  late double _z;
-
+class _painter extends CustomPainter {
 
   @override
-  void initState() {
-    _x = 0.0;
-    _y = 0.0;
-    _z = 0.0;
+  void paint(Canvas canvas, Size size) {
+    //All our work will be done
+
+    final radius = 100.0;
+    final Offset offset = new Offset(0.0, 75.0);
+    final Paint paint = new Paint()
+      ..isAntiAlias = true
+      ..strokeWidth = 10.0
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke;
+
+    final Offset bodystart = new Offset(0.0, 50.0);
+    final Offset bodyend = new Offset(0.0, 0.0);
+
+    canvas.drawCircle(offset, radius, paint);
+    canvas.drawLine(bodystart, bodyend, paint);
+
+    final Rect rect = new Rect.fromCircle(center: offset, radius: radius);
+    final Rect smallrect = new Rect.fromCircle(center: offset, radius: 50.0);
+    final Paint rectpaint = new Paint()
+      ..isAntiAlias = true
+      ..strokeWidth = 10.0
+      ..color = Colors.orange
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawRect(rect, rectpaint);
+    canvas.drawRect(smallrect, rectpaint);
   }
-  
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Flutter Transformations'),
+        title: new Text('Name here'),
       ),
       body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              new Row(children: <Widget>[
-                new Text('X'),
-                new Slider(value: _x, onChanged: (double value) => setState(() => _x = value))
-              ],),
-              new Row(children: <Widget>[
-                new Text('Y'),
-                new Slider(value: _y, onChanged: (double value) => setState(() => _y = value))
-              ],),
-              new Row(children: <Widget>[
-                new Text('Z'),
-                new Slider(value: _z, onChanged: (double value) => setState(() => _z = value))
-              ],),
-              new Transform(transform: new Matrix4.skewY(_y),
-                child: new Transform(transform: new Matrix4.skewX(_x),
-                child: new Transform(transform: new Matrix4.rotationZ(_z),
-                child: new Padding(padding: new EdgeInsets.all(10.0), child: new Text('hello World'),),),),
-              )
-            ],
-          ),
-        )
+          padding: new EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+
+
+                new Text('Hello', style: new TextStyle(fontSize: 30.0, fontWeight:  FontWeight.bold),),
+                new CustomPaint(painter: new _painter(),),
+              ],
+            ),
+          )
       ),
     );
   }
