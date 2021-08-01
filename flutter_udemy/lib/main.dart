ffa935f0c777980068a81b1ea5fe12cf6c6fda77
wasimiqbal1 <wasim_iqbal@msn.com>
2021-08-01 14:55:22 +0500

    Opacity

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -1,23 +1,50 @@
 import 'package:flutter/material.dart';
-import './screens/home.dart';
-import './screens/second.dart';
-import './screens/third.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+
+  late bool _visible;
+
+
+  @override
+  void initState() {
+    _visible = true;
+  }
+
+  void _toggleVisible() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Navigation',
-      routes: <String, WidgetBuilder>{
-        //All available pages
-        '/Home': (BuildContext contrext) => new Home(),
-        '/Second': (BuildContext contrext) => new Second(),
-        '/Third': (BuildContext contrext) => new Third(),
-      },
-      home: new Home(), //first page displayed
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: Center(
+          child: new Column(
+            children: <Widget>[
+              new Opacity(opacity: _visible ? 1.0 : 0.2,
+              child: new Text('Now you see me now you don\'t!')),
+              new ElevatedButton(onPressed: _toggleVisible, child: new Text('Toggle'),)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
