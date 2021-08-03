50c7c4bc120c6b77d34e45468f2597e627f6e8d5
wasimiqbal1 <wasim_iqbal@msn.com>
2021-08-04 00:06:12 +0500

    Flutter Animation

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/animation.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -12,9 +11,9 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
-class _State extends State<MyApp> with SingleTickerProviderStateMixin {
+class _State extends State<MyApp> with TickerProviderStateMixin {
 
-  late Animation<double> animation;
+  late Animation animation;
   late AnimationController controller;
 
 
@@ -22,36 +21,40 @@ class _State extends State<MyApp> with SingleTickerProviderStateMixin {
   void initState() {
     super.initState();
 
-    controller = new AnimationController( duration: const Duration(milliseconds: 5000),vsync: this);
-    animation = new Tween(begin: 0.0, end: 200.0).animate(controller);
-    animation.addListener(() {
-      setState(() {
-        //The state of the animation has changed
-      });
-    });
+    controller = new AnimationController(duration: const Duration(milliseconds: 5000),vsync: this);
 
+    final CurvedAnimation curve = new CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    animation = new Tween(begin: 100.0, end: 300.0).animate(curve);
+    animation.addStatusListener(listener);
     controller.forward();
   }
 
+  void listener(AnimationStatus status) {
+    if(status == AnimationStatus.completed) {
+      controller.reverse();
+    } else if(status == AnimationStatus.dismissed) {
+      controller.forward();
+    }
+  }
 
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  Widget builder(BuildContext context, Widget child) {
+    return new Container(
+      height: animation.value,
+      width: animation.value,
+      child: new FlutterLogo(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Flutter Animations'),
+        title: new Text('Name here'),
       ),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
-        height: animation.value,
-        width: animation.value,
         child: new Center(
-          child: new FlutterLogo(size: 300.0,)
+          child: new AnimatedBuilder(animation: animation, builder: builder),
         )
       ),
     );
